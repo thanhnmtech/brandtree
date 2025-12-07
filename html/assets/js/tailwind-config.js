72ec2432d523b6ec3025008c29d49f4a95962120
Sonic Lee <sonlocle36123@gmail.com>
2025-12-07 22:29:28 +0700

    .

---
+++ html/assets/js/tailwind-config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  prefix: "tw-",
+  theme: {
+    extend: {
+      fontFamily: {
+        bevietnam: ["Be Vietnam Pro", "sans-serif"],
+      },
+      colors: {
+        vlbcgreen: "#16a249",
+      },
+    },
+  },
+};
