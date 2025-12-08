16ee3186bf1c3923f729016f952e335ca293080a
Khanh <Khanhks2080@gmail.com>
2025-12-08 08:58:36 +0700

    update html

--- public/html/assets/js/tailwind-config.js
+++ public/html/assets/js/tailwind-config.js
@@ -1,13 +1,17 @@
 tailwind.config = {
-  prefix: "tw-",
-  theme: {
-    extend: {
-      fontFamily: {
-        bevietnam: ["Be Vietnam Pro", "sans-serif"],
-      },
-      colors: {
-        vlbcgreen: "#16a249",
-      },
+    prefix: "tw-",
+    theme: {
+        extend: {
+            fontFamily: {
+                bevietnam: ["Be Vietnam Pro", "sans-serif"],
+            },
+            colors: {
+                vlbcgreen: "#16a249",
+                beige: "#F5F3EF",
+                vlbcGreen1: "#1C6B42",
+                vlbcGreen2: "#2A7C4A",
+                vlbcReady: "#5ED27A",
+            },
+        },
     },
-  },
 };
